92e031738b7da734575131332be1b57c48626af1
MahmudK <mahmudkenway@gmail.com>
2022-05-07 17:20:43 +0430

    create controls component for control buttons

---
+++ src/Components/Controls.js
@@ -0,0 +1,15 @@
+import "./Controls.css";
+
+function Controls() {
+
+
+    return (
+        <div>
+            <button className="sq-btn"></button>
+            <button className="sq-btn"></button>
+            <button className="sq-btn"></button>
+        </div>
+    )
+}
+
+export default Controls;
