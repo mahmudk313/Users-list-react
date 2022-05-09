7824c9f32cffeb3af829f107a57baf4d546f3744
MahmudK <mahmudkenway@gmail.com>
2022-05-09 20:07:11 +0430

    ListShow component modified, table created, state defined
    List show changes:
    - ListShow style added
    - table created
    - state defined
    - state users defined

--- src/Components/ListShow.js
+++ src/Components/ListShow.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import './ListShow.css';
 
 function ListShow() {
 
+    const [state, setState] = useState({
+        users : [
+            // State.users Props
+            // {
+            //     name : 'mahmud',
+            //     lastName : 'bak',
+            //     permission : 'admin',
+            //     joinDate : '1400',
+            //     email : 'mkm',
+            //     id : 1,
+            //     skill : 1,
+            //     detail : 1
+            // }
+        ]
+    });
+
+
+    return(
+        <div className="list-show">
+            <table>
+                <tbody>
+                    <tr>
+                        <th>نام</th>
+                        <th>نام خانوادگی</th>
+                        <th>دسترسی</th>
+                        <th>تاریخ عضویت</th>
+                        <th>ایمیل</th>
+                        <th>شناسه</th>
+                        <th>مهارت</th>
+                        <th>توضیحات</th>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default ListShow;
